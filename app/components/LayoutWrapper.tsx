3b4a0ad598ee1e958c44ee6ffc7f37f4eb5060a5
langjihao <langjihao@gmail.com>
2024-09-06 13:53:15 +0800

    chore: Update API base URL to use localhost for development

--- app/components/LayoutWrapper.tsx
+++ app/components/LayoutWrapper.tsx
@@ -1,7 +1,5 @@
 'use client'
-import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import { useState } from 'react';
-import { UserCircleIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
 
