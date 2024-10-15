93c82585423b74fac0f123799b3e92328b812867
langjihao <langjihao@gmail.com>
2024-10-15 17:30:37 +0800

    fixbug

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [ {
-        protocol: 'https',
-        hostname: 'img.clerk.com',
-        port: '',        
-      },],
-  },
-};
-
-module.exports = nextConfig;
+module.exports = {
+  output: 'export',
+}
